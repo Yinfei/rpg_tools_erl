4c2b55eba367cc5f848a1146268814e8d52afb03
Yohan Piron <Yinfei84@gmail.com>
2015-06-18 23:55:11 +0200

    Added coin flip

--- rpg_tools.erl
+++ rpg_tools.erl
@@ -1,9 +1,18 @@
 -module(rpg_tools).
 -export([roll_dice/1]).
 -export([roll_dice_batch/2]).
+-export([flip_coin/0]).
 
 roll_dice(X) ->
   random:uniform(X).
 
 roll_dice_batch(X, Y) ->
   [random:uniform(Y) || _ <- lists:seq(1, X)].
+
+flip_coin() ->
+  case random:uniform(2) == 2 of
+      true ->
+          "Heads !";
+      _Else ->
+          "Tails!"
+  end.

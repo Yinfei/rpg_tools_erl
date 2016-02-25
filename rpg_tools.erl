edc3fdeb7e1c365f9e5377662ce4d1f84d5ad429
Yohan Piron <Yinfei84@gmail.com>
2016-02-25 16:47:02 +0100

    Add modifiers

--- rpg_tools.erl
+++ rpg_tools.erl
@@ -1,14 +1,22 @@
 -module(rpg_tools).
 -export([roll_dice/1]).
+-export([roll_dice/2]).
 -export([roll_dice_batch/2]).
+-export([roll_dice_batch/3]).
 -export([flip_coin/0]).
 -export([flip_coin/1]).
 
-roll_dice(X) ->
-    random:uniform(X).
+roll_dice(MaxValue) ->
+    random:uniform(MaxValue).
 
-roll_dice_batch(X, Y) ->
-    [random:uniform(Y) || _ <- lists:seq(1, X)].
+roll_dice(MaxValue, Modifier) ->
+    Modifier + random:uniform(MaxValue).
+
+roll_dice_batch(BatchSize, MaxValue) ->
+    [random:uniform(MaxValue) || _ <- lists:seq(1, BatchSize)].
+
+roll_dice_batch(BatchSize, MaxValue, Modifier) ->
+    [Modifier + N || N <- roll_dice_batch(SizeSize, MaxValue)].
 
 flip_coin() ->
     flip_coin(random:uniform(2)).

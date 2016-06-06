d6cab5bc4548bae2d2c430dc80279e8e35089835
Yohan Piron <yinfei84@gmail.com>
2016-06-06 16:50:32 +0200

    rename pick_random for a less confusing name

--- apps/rpg_tools_erl/src/card.erl
+++ apps/rpg_tools_erl/src/card.erl
@@ -1,7 +1,7 @@
 -module(card).
 -export([pick/1]).
 -export([pick_number/1]).
--export([pick_random/1]).
+-export([pick_random_list_element/1]).
 -define(HEADS, ["Jack", "Queen", "King", "Ace"]).
 -define(COLORS, ["Clubs", "Hearts", "Diamonds", "Spades"]).
 -define(DECK_32, lists:append(lists:seq(7, 10), ?HEADS)).
@@ -10,13 +10,13 @@
 pick(DeckSize) ->
     CardNumber = pick_number(DeckSize),
 
-    lists:concat([CardNumber, ' of ', pick_random(?COLORS),  '.']).
+    lists:concat([CardNumber, ' of ', pick_random_list_element(?COLORS),  '.']).
 
 pick_number(MaxSize) ->
-    if MaxSize =:= 32 -> pick_random(?DECK_32);
-       true -> pick_random(?DECK_54)
+    if MaxSize =:= 32 -> pick_random_list_element(?DECK_32);
+       true -> pick_random_list_element(?DECK_54)
     end.
 
-pick_random(List) ->
+pick_random_list_element(List) ->
     Index = random:uniform(length(List)),
     lists:nth(Index, List).
